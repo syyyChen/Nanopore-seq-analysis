95d00b466691f6645244800632f0f1d182db4eea
Shiyao Chen <sychen0927@gmail.com>
2023-11-06 11:03:29 +0100

    add pattern detection functions

--- reads_extraction.py
+++ reads_extraction.py
@@ -94,44 +94,31 @@ def mask_regions(vsearch_res_df, fasta, tmp_dir):
     SeqIO.write(masked_records, os.path.join(tmp_dir, "masked.fasta"), "fasta")
     
 
-def iterative_vsearch(input_fasta, adapters_fasta, output, tmp_dir, id=0.7, rounds=3, threads=1):
-    
+def iterative_vsearch(input_fasta, adapters_fasta, output, id=0.7, rounds=3, threads=1):
     query = input_fasta
-    tmp_fasta = os.path.join(tmp_dir, "tmp.fasta")
     res_all = pl.DataFrame(schema=schema)
     
-    for round in range(1, rounds+1):
-        result = os.path.join(tmp_dir, "vsearch_res_{no}.tsv".format(no=round))
-        vsearch_cmd = "vsearch --usearch_global {query_fasta} --db {adapters} --minseqlength 20 --maxaccepts 5 --id {min_id} --strand plus \
-        --wordlength 3 --minwordmatches 10 --threads {nthreads} --userfields \
-        'query+target+id+alnlen+mism+opens+qilo+qihi+qstrand+tilo+tihi+ql+tl' \
-        --userout {out} --matched {tmp_file}".format(
-            query_fasta=query,
-            adapters=adapters_fasta,
-            min_id=id,
-            nthreads=threads,
-            out=result,
-            tmp_file=tmp_fasta
-        )
-        if round == 1:
-            vsearch_cmd += " --output_no_hits"
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        for round in range(1, rounds+1):
+            result = os.path.join(tmp_dir, f"vsearch_res_{round}.tsv")
+            tmp_fasta = os.path.join(tmp_dir, "tmp.fasta")
+            vsearch_cmd = f"vsearch --usearch_global {query} --db {adapters_fasta} \
+            --minseqlength 20 --maxaccepts 5 --id {id} --strand plus \
+            --wordlength 3 --minwordmatches 10 --threads {threads} --userfields \
+            'query+target+id+alnlen+mism+opens+qilo+qihi+qstrand+tilo+tihi+ql+tl' \
+            --userout {result} --matched {tmp_fasta}"
+            if round == 1:
+                vsearch_cmd += " --output_no_hits"
+                
+            subprocess.run(vsearch_cmd, shell=True, check=True)
             
-        p = subprocess.Popen(vsearch_cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-        p.communicate()
+            if not os.path.getsize(result):
+                break
+            vsearch_results = read_vsearch_res(result)
+            res_all = pl.concat([res_all, vsearch_results])
+            if round < rounds:
+                mask_regions(vsearch_results, tmp_fasta, tmp_dir)
+                query = os.path.join(tmp_dir,"masked.fasta")
         
-        # stop the iteration if there are no more targets found
-        if not os.path.getsize(result):
-            break
-        vsearch_results = read_vsearch_res(result)
-        res_all = pl.concat([res_all, vsearch_results])
-        # reads found at least one target will be used for the next round of searching
-        # targets already found are masked with U
-        if round < rounds:
-            mask_regions(vsearch_results, tmp_fasta, tmp_dir)
-            subprocess.run("rm {tmp_fasta}".format(tmp_fasta=tmp_fasta), shell=True)
-            query = os.path.join(tmp_dir, "masked.fasta")
-        
-    subprocess.run("rm -r {tmp}/*".format(tmp=tmp_dir), shell=True)
-    res_all=res_all.sort(["query", "qilo"])
-    res_all.write_csv(output, separator="\t", has_header=False)
-    
+        res_all = res_all.sort(["query", "qilo"])
+        res_all.write_csv(output, separator="\t", has_header=False)
