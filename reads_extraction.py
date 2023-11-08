6f651f8419c19373aa4b861834e4f8e7df5a5ded
Shiyao Chen <sychen0927@gmail.com>
2023-11-08 09:36:04 +0100

    add config statistics

--- reads_extraction.py
+++ reads_extraction.py
@@ -70,7 +70,7 @@ def write_adapters_fasta(adapter1_seq, adapter2_seq, output):
 
 
 def read_vsearch_result(tsv_file, selected_columns=vsearch_colnames):
-    
+    """Read vsearch results as a polars dataframe"""
     for col in selected_columns:
         if col not in schema:
             raise ValueError(f"Invalid column name: {col}")
@@ -89,10 +89,10 @@ def read_vsearch_result(tsv_file, selected_columns=vsearch_colnames):
     return df
 
 
-def mask_regions(vsearch_res_df, fasta, tmp_dir):
-    
+def mask_regions(vs_result_df, fasta, tmp_dir):
+    """Mask mapped regions of reads with 'U' according to vsearch results """
     regions_to_mask = defaultdict(list)
-    for row in vsearch_res_df.iter_rows():
+    for row in vs_result_df.iter_rows():
         if row[11] == 0:
             continue
         query_id = row[0]
@@ -110,65 +110,95 @@ def mask_regions(vsearch_res_df, fasta, tmp_dir):
     SeqIO.write(masked_records, os.path.join(tmp_dir, "masked.fasta"), "fasta")
 
 
-def iterative_vsearch(input_bam, adapter1, adapter2, output_dir, min_len=20, id=0.7, rounds=3, threads=1):
+def iterative_vsearch(input_bam, adapters_fasta, output_dir, tmp_dir, min_len=20, id=0.7, rounds=3, threads=1):
+    """Do iterative vsearch to find all adapters in the reads for pattern identification"""
+    
     output = os.path.join(output_dir, "adapter_scan.vsearch.tsv")
     res_all = pl.DataFrame(schema=schema)
-    
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        adapters_fasta = os.path.join(tmp_dir, "adapters.fasta")
-        write_adapters_fasta(adapter1, adapter2, adapters_fasta)
-        query = os.path.join(tmp_dir, "input.fasta")
-        subprocess.run(f"samtools fasta {input_bam} > {query}", shell=True)
+    query = os.path.join(tmp_dir, "input.fasta")
+    subprocess.run(f"samtools fasta {input_bam} > {query}", shell=True)
         
-        for round in range(1, rounds+1):
-            result = os.path.join(tmp_dir, f"vsearch_res_{round}.tsv")
-            tmp_fasta = os.path.join(tmp_dir, "tmp.fasta")
-            vsearch_cmd = f"vsearch --usearch_global {query} --db {adapters_fasta} \
-            --minseqlength {min_len} --maxaccepts 5 --id {id} --strand plus \
-            --wordlength 3 --minwordmatches 10 --threads {threads} --userfields \
-            'query+target+id+alnlen+mism+opens+qilo+qihi+qstrand+tilo+tihi+ql+tl' \
-            --userout {result} --matched {tmp_fasta}"
-            if round == 1:
-                vsearch_cmd += " --output_no_hits"
-                
-            subprocess.run(vsearch_cmd, shell=True, check=True)
+    for round in range(1, rounds+1):
+        result = os.path.join(tmp_dir, f"vsearch_res_{round}.tsv")
+        tmp_fasta = os.path.join(tmp_dir, "tmp.fasta")
+        vsearch_cmd = f"vsearch --usearch_global {query} --db {adapters_fasta} \
+        --minseqlength {min_len} --maxaccepts 5 --id {id} --strand plus \
+        --wordlength 3 --minwordmatches 10 --threads {threads} --userfields \
+        'query+target+id+alnlen+mism+opens+qilo+qihi+qstrand+tilo+tihi+ql+tl' \
+        --userout {result} --matched {tmp_fasta}"
+        if round == 1:
+            vsearch_cmd += " --output_no_hits"
             
-            if not os.path.getsize(result):
-                break
-            vsearch_results = read_vsearch_result(result)
-            res_all = pl.concat([res_all, vsearch_results])
-            if round < rounds:
-                mask_regions(vsearch_results, tmp_fasta, tmp_dir)
-                query = os.path.join(tmp_dir,"masked.fasta")
+        subprocess.run(vsearch_cmd, shell=True, check=True)
         
-        res_all = res_all.sort(["query", "qilo"])
-        res_all.write_csv(output, separator="\t", has_header=False)
-        return res_all
+        if not os.path.getsize(result):
+            break
+        vsearch_results = read_vsearch_result(result)
+        res_all = pl.concat([res_all, vsearch_results])
+        if round < rounds:
+            mask_regions(vsearch_results, tmp_fasta, tmp_dir)
+            query = os.path.join(tmp_dir,"masked.fasta")
+    
+    res_all = res_all.sort(["query", "qilo"])
+    res_all.write_csv(output, separator="\t", has_header=False)
+    return res_all
+
+
+def collect_config_statistics(vs_result, output_dir):
+    """Collect statistics of adapter patterns in one read"""
+    grouped_res = vs_result.groupby('query').agg(
+        pl.col("target").str.concat("-").alias("Configuration"))
+
+    # Calculate the occurrences and frequencies
+    config_counts = grouped_res.groupby("Configuration").agg(
+        pl.count().alias("Occurrences")
+    ).with_columns(Frequency=(pl.col.Occurrences / grouped_res.height) * 100)
+
+    config_counts = config_counts.sort("Frequency", descending=True)
+    config_counts.write_csv(os.path.join(output_dir, 'config_stat.csv'))
 
 
 def find_valid_pairs(vs_result):
+    """Identify valid pairs and get the position of the reads to be extracted"""
+    print("Finding valid pairs...")
     valid_pairs = {}
-    for query_id, grouped_df in vs_result.group_by('query'):
-        grouped_df = grouped_df.sort('qilo')
-        if grouped_df['target'][0] == '*':
-            continue
-        for i in range(grouped_df.height - 1):
-            current_adapter = grouped_df.row(i)
-            next_adapter = grouped_df.row(i + 1)
-            if (current_adapter[1] in valid_patterns and
-                valid_patterns[current_adapter[1]] == next_adapter[1] and
-                current_adapter[7] < next_adapter[6]):
+    n_valid = 0
+    n_reads = 0
+
+    vs_result = vs_result.sort('qilo')
+    for query_id, grouped_df in vs_result.filter(pl.col('target') != '*').group_by('query'):
+        n_reads += 1
+
+        targets = grouped_df['target'].to_list()
+        qihis = grouped_df['qihi'].to_list()
+        qilos = grouped_df['qilo'].to_list()
+
+        for i in range(len(targets) - 1):
+            current_target = targets[i]
+            next_target = targets[i + 1]
+            current_qihi = qihis[i]
+            next_qilo = qilos[i + 1]
+
+            if (current_target in valid_patterns and
+                valid_patterns[current_target] == next_target and
+                current_qihi < next_qilo - 1):
                 valid_pairs.setdefault(query_id, [])
-                valid_pairs[query_id].append((current_adapter[7], next_adapter[6] - 1))
+                valid_pairs[query_id].append((current_qihi, next_qilo - 1))
+                n_valid += 1
+
+    n_unpaired = vs_result.filter(pl.col('target') != '*').height - 2 * n_valid
+    print(f"Found {n_valid} valid pairs in {n_reads} reads. {n_unpaired} adapters cannot be paired.")
     return valid_pairs
 
 
 def extract_reads(input_bam, valid_pairs_dict, output_dir):
-    
+    """Extract reads from the bam file. Maintain all other information."""
+    print("Extracting reads...")
+    n_reads = 0
     output_bam = os.path.join(output_dir, "extracted_reads.bam")
-    with pysam.AlignmentFile(input_bam, "rb", check_sq=False) as bamfile:
-        
+    with pysam.AlignmentFile(input_bam, "rb", check_sq=False) as bamfile:    
         with pysam.AlignmentFile(output_bam, "wb", header=bamfile.header) as outfile:
+            
             for read in bamfile.fetch(until_eof=True):
                 query_name = read.query_name
                 if query_name in valid_pairs_dict:
@@ -180,6 +210,8 @@ def extract_reads(input_bam, valid_pairs_dict, output_dir):
                         new_read.qual = read.qual[start:end] if read.qual else None
                         new_read.tags = read.tags 
                         outfile.write(new_read)
+                        n_reads += 1
+    print(f"{n_reads} reads with pattern have been extracted")
 
 
 def length_distribution_plot(valid_pairs_dict, output_dir):
@@ -200,7 +232,7 @@ def length_distribution_plot(valid_pairs_dict, output_dir):
     plt.title('Length Distribution of Extracted Reads')  
     plt.xlabel('Length')  
     plt.ylabel('Frequency')  
-    plt.xticks(np.arange(min(lengths), max(lengths) + 200, 200))
+    plt.xticks(np.arange(0, (2 + max(lengths) // 400) * 400, 400))
     plt.axvline(median_length, color='red', linestyle='dashed', linewidth=1)
     plt.axvline(average_length, color='orange', linestyle='dashed', linewidth=1)
     plt.text(median_length, max(frequencies)*0.8, f'Median: {median_length:.2f}', color='red')
@@ -213,17 +245,25 @@ def length_distribution_plot(valid_pairs_dict, output_dir):
 
 
 def main(args):
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+        
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        adapters_fasta = os.path.join(tmp_dir, "adapters.fasta")
+        write_adapters_fasta(args.adapter1, args.adapter2, adapters_fasta)
+        
+        vsearch_results = iterative_vsearch(
+            input_bam=args.input_bam,
+            adapters_fasta=adapters_fasta,
+            output_dir=args.output_dir,
+            tmp_dir=tmp_dir,
+            min_len=args.min_len,
+            id=args.identity,
+            rounds=args.rounds,
+            threads=args.threads
+        )
     
-    vsearch_results = iterative_vsearch(
-        input_bam=args.input_bam,
-        adapter1=args.adapter1,
-        adapter2=args.adapter2,
-        output_dir=args.output_dir,
-        min_len=args.min_len,
-        id=args.identity,
-        rounds=args.rounds,
-        threads=args.threads
-    )
+    collect_config_statistics(vsearch_results, args.output_dir)
 
     valid_pairs_dict = find_valid_pairs(vsearch_results)
 
@@ -244,14 +284,13 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Extract reads from basecalled bam file and collect statistics.")
     parser.add_argument("-i", "--input_bam", required=True, help="Input BAM file path")
-    parser.add_argument("-a1", "--adapter1", required=True, help="Sequence of the first adapter")
-    parser.add_argument("-a2", "--adapter2", required=True, help="Sequence of the second adapter")
+    parser.add_argument("-a1", "--adapter1", required=True, help="Sequence of the first adapter on the 5' end")
+    parser.add_argument("-a2", "--adapter2", required=True, help="Sequence of the second adapter on the 3' end")
     parser.add_argument("-o", "--output_dir", required=True, help="Directory to save output files")
-    parser.add_argument("-ml", "--min_len", type=int, default=20, help="Minimum length of reads to consider by vsearch")
+    parser.add_argument("-ml", "--min_len", type=int, default=20, help="Minimum length of reads to be considered by vsearch")
     parser.add_argument("-id", "--identity", type=float, default=0.7, help="Minimum identity for adapter scanning")
     parser.add_argument("-r", "--rounds", type=int, default=3, help="Number of iterative rounds for vsearch")
-    parser.add_argument("-t", "--threads", type=int, default=1, help="Number of threads for vsearch")
-
+    parser.add_argument("-t", "--threads", type=int, default=1, help="Number of threads of vsearch")
     args = parser.parse_args()
     main(args)
 
